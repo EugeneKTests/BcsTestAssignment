d17dcdf097029149bec0c9b237fe6194d7903596
eugenek <Seriouspk@yandex.ru>
2022-03-20 14:47:45 +0300

    use application.yml

--- src/main/java/bcs/testassignment/client/IexCloudClient.java
+++ src/main/java/bcs/testassignment/client/IexCloudClient.java
@@ -5,9 +5,7 @@ import bcs.testassignment.model.Quote;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
 
 @Component
 @RequiredArgsConstructor
